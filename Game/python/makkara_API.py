88c2cfee58fd865259df8a44e37577d0b950ebd0
MustBeViable <elias1rinne@gmail.com>
2024-11-29 11:25:56 +0200

    fixed bug: If robber happened, it gave internal server error - fix: did not return anything if player had 0 money
    Updated:
    - airport query: saves list of airports to backend
    - airport_selected: gets data from backend, up dates player money and location. Returns only chosen airport info. Is not meant to be used in front end (add something if you want it to return ie boolean value)

--- Game/python/makkara_API.py
+++ Game/python/makkara_API.py
@@ -1,15 +1,21 @@
+from wsgiref.util import request_uri
+
 from flask import Flask
 from flask_cors import CORS
 from Game.python.airport_selection_function import airportselection
 from Game.python.garbage_can import garbage_can, finnair_personnel, money_from_garbage, robber, hole_in_charge
+from Game.python.sql_querys.money_function import update_player_money, fetch_player_money
 from Game.python.sql_querys.player_location_fetch_and_update_querys import update_player_location, fetch_player_location
-
+import json,tempfile
 app = Flask(__name__)
+
+airports = {}
 
 @app.route('/airport/<ide>')
 #Gives dictionaries within dictionary of 20 randomized airports based on location to user
 def airport(ide):
     result = airportselection(ide)
+    airports[ide] = result
     return result
 
 @app.route('/garbage/<ide>')
@@ -38,13 +44,20 @@ def finnair(ide, answer):
 #Returns dictionary like this {'answer': 'Ei rahaa'}
     if answer == 'true':
         result = finnair_personnel(ide, answer)
-        print(type(answer))
         return result
 
-@app.route('/airport_selected/<ide>/<icao>')
-def airport_selected(ide, icao):
-    update_player_location(ide, icao)
-    result = fetch_player_location(ide)
+@app.route('/airport_selected/<ide>/<airport_num>')
+#Get users selected airport (frond end checks if he can afford) and updates player location. Also takes the fee here.
+#User cannot change the values (ie. prices), list is saved here. Returned result is just meant for checking does all go
+#as planned
+def airport_selected(ide, airport_num):
+    result = airports[ide][airport_num]
+    ariport_icao = result['ident']
+    price = result['price']
+    spend = int(fetch_player_money(1)) - int(price)
+    update_player_money(spend, ide)
+    update_player_location(ide, ariport_icao)
+    return result
 
 
 CORS(app)
@@ -52,3 +65,11 @@ CORS(app)
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
+
+'''    
+    update_player_location(ide, icao)
+    result = fetch_player_location(ide)
+    
+        tfile=result
+    config = {}
+    json.dump(config, tfile)'''
