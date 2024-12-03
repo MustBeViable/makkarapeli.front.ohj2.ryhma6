6e28d37e0a7abfd3aa0d381b0bda43913cee3d6b
Aleksi <aleklap@metropolia.fi>
2024-12-03 11:56:07 +0200

    hopeful push

--- Game/Javascript/airport_selection.js
+++ Game/Javascript/airport_selection.js
@@ -15,13 +15,27 @@ async function getData(url){
   }
 }
 
+async function get_money(){
+  //add IDE functionality to all functions when it works
+  const url = `http://127.0.0.1:5000/player_money/1`
+  try {
+    const result = await getData(url)
+    console.log(result)
+    console.log(typeof result)
+    return result
+  }
+  catch (error){
+
+  }
+}
+
 async function airport_fly_to(airport_number){
-  //here should be a call to check players money
-  //if money is not enough for flight returns a message that reflects that
 
   try{
 
     const url = `http://127.0.0.1:5000/airport_selected/1/${airport_number}`
+    const result = await getData(url)
+    return;
 
   }
   catch (error){
@@ -31,7 +45,7 @@ async function airport_fly_to(airport_number){
 }
 
 
-async function airport_selection_function(evt){
+async function airport_selection_function(){
   try {
     const url = "http://127.0.0.1:5000/airport/1";
     const result = await getData(url)
@@ -45,8 +59,21 @@ async function airport_selection_function(evt){
       button.setAttribute("id", buttonid)
       button.style.display = "block"
       button.addEventListener("click",() =>{
+        //here should be a call to check players money
+        //if money is not enough for flight returns a message that reflects that
+
+        const result1 = get_money()
+        console.log(get_money())
+        if (result1 < Number(result[`${i+1}`].price)){
+          console.log("not enough money");
+          return;
+        }
         // needs a function that calls for some kind of flyto api
         // so an async function
+        console.log(result[`${i + 1}`]['number'])
+        airport_fly_to(result[`${i + 1}`]['number'])
+
+
         console.log(button.id)
       })
       document.querySelector("#target12").appendChild(button)
