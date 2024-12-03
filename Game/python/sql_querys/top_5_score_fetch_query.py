a8d3e63815c81d894b319389db3b2756dacdf144
MustBeViable <elias1rinne@gmail.com>
2024-12-03 11:51:18 +0200

    taxfree

--- Game/python/sql_querys/top_5_score_fetch_query.py
+++ Game/python/sql_querys/top_5_score_fetch_query.py
@@ -8,12 +8,12 @@ def print_all_players_top():
     kursori.execute(sql)
     result = kursori.fetchall()
     result_sorted = sorted(result, key=lambda x: x['score'], reverse=True)
-    print(f"Kaikkien pelaajien Top 5 tulokset: ")
     if len(result_sorted) < 5:
-        for i in range(len(result_sorted)):
-            print(f"{i + 1}. {result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f} pt")
+        return result_sorted
     else:
+        top_5 = {}
         for i in range(5):
-             print(f"{i + 1}. {result_sorted[i]['screen_name']}: {result_sorted[i]['score']:6.0f} pt")
-    print("")
-    return
+             top_5[i+1] = result_sorted[i]
+        return top_5
+
+print(print_all_players_top())
