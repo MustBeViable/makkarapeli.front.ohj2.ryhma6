3594af175d2d296a61663a5b1c0da4b023b5ed1b
tuukka <tuukkauu@metropolia.fi>
2024-12-05 15:21:02 +0200

    kaikenlaista korjattu

--- Game/Javascript/oikea_taxfree.js
+++ Game/Javascript/oikea_taxfree.js
@@ -10,9 +10,9 @@ async function getDataTaxfree (ide, method) {
   }
 }
 
-async function buySausage (ide, method) {
+async function buySausage (ide) {
   try {
-    const response = await fetch(`http://127.0.0.1:5000/taxfree_buy/${ide}/${method}`)
+    const response = await fetch(`http://127.0.0.1:5000/taxfree_buy/${ide}`)
     const json = await response.json();
     return json
   } catch (error) {
@@ -20,30 +20,28 @@ async function buySausage (ide, method) {
   }
 }
 
-const ide = '1'
-
-const dialog = document.getElementById('taxfree');
-const button = document.querySelector('#taxfree_btn');
-const button2 = document.querySelector('#close');
-const button3 = document.getElementById('taxfree_buy_sausage')
+const dialog_taxfree = document.getElementById('taxfree');
+const button_taxfree = document.querySelector('#taxfree_btn');
+const close_taxfree = document.querySelector('#close_taxfree');
+const buy_sausage = document.getElementById('taxfree_buy_sausage')
 
 
 
-button.addEventListener('click',  (evt) => {
-  dialog.showModal()
+button_taxfree.addEventListener('click',  (evt) => {
+  dialog_taxfree.showModal()
 })
 
-button2.addEventListener('click', (evt) => {
-  dialog.close();
-  const method = 'close';
+close_taxfree.addEventListener('click', (evt) => {
+  dialog_taxfree.close();
 })
 
-button3.addEventListener('click', (evt) => {
+buy_sausage.addEventListener('click', (evt) => {
   console.log('Tehdään kaupat');
   const method = 'payment';
+  buySausage(ide)
 })
 
-button.addEventListener('click', async(evt) => {
+button_taxfree.addEventListener('click', async(evt) => {
   console.log('Tervetuloa ostoksille!')
   const img = document.querySelector('#bagdrop')
   img.src = `/Game/images_and_other/taxfree.png?random=${Date.now()}`
