2b7f0f919b98b41c8891fd96be882a2504d404d6
senja <senja.vuoristo@aalto.fi>
2024-12-10 15:42:21 +0200

    Try preventing dialog closing (doesn't work yet)

--- Game/Javascript/login_ui.js
+++ Game/Javascript/login_ui.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const target = document.getElementById('login_target');
-const header = document.getElementById('loginheader')
+const loginBase = document.getElementById('login_target');
+const loginHeader = document.getElementById('loginheader');
+const loginDialog = document.getElementById('login');
 const signInText = 'Kirjaudu';
 const signUpText = 'Luo tunnus';
-const signUpButtonText = 'Oletko uusi? Luo käyttäjätili'
-const signInButtonText = "Onko sinulla jo tili? Kirjaudu sisään"
+const signUpButtonText = 'Oletko uusi? Luo käyttäjätili';
+const signInButtonText = 'Onko sinulla jo tili? Kirjaudu sisään';
 const signInHeader = 'Kirjaudu sisään';
 const signUpHeader = 'Rekisteröidy';
+
+/**
+ * Prevent user from using esc to close a dialog.
+ */
+function preventCancel(event) {
+  console.log('t')
+    if (event.key === 'Escape') {
+      event.preventDefault();
+      console.log('Esc-näppäimen toiminta estetty');
+    }
+  }
+
 
 /**
  * Creates a button and adds an action to it.
@@ -15,10 +28,10 @@ const signUpHeader = 'Rekisteröidy';
  * @param {function} onClick Action on click
  */
 function createButton(text, onClick) {
-  const button = document.createElement('button');
-  button.textContent = text;
-  target.appendChild(button);
-  button.addEventListener('click', onClick);
+  const loginButton = document.createElement('button');
+  loginButton.textContent = text;
+  loginBase.appendChild(loginButton);
+  loginButton.addEventListener('click', onClick);
 }
 
 /**
@@ -32,21 +45,21 @@ function createNameForm(value, signin) {
       <input id="nameQuery" name="q" type="text" placeholder="Käyttäjätunnus">
     </form>
   `;
-  target.innerHTML += formHtml;
+  loginBase.innerHTML += formHtml;
 
   const screenNameForm = document.getElementById('nameForm');
 
   if (!signin) {
-    const input2 = document.createElement('input');
-    input2.type = 'text';
-    input2.name = 'shoeSize';
-    input2.placeholder = 'Kengännumero';
-    screenNameForm.appendChild(input2);
+    const extraformInput = document.createElement('input');
+    extraformInput.type = 'text';
+    extraformInput.name = 'shoeSize';
+    extraformInput.placeholder = 'Kengännumero';
+    screenNameForm.appendChild(extraformInput);
   }
-    const submitButton = document.createElement('input');
-    submitButton.type = 'submit';
-    submitButton.value = value;
-    screenNameForm.appendChild(submitButton);
+  const submitButton = document.createElement('input');
+  submitButton.type = 'submit';
+  submitButton.value = value;
+  screenNameForm.appendChild(submitButton);
 
   screenNameForm.addEventListener('submit', async (event) => {
     const screenName = document.getElementById('nameQuery').value;
@@ -55,31 +68,33 @@ function createNameForm(value, signin) {
   });
 }
 
-
 /**
  * Creates the sign-in page with screen name form and a button that takes user to sign-up page.
  */
 function createSigninPage() {
-  header.innerText = signInHeader
+  loginHeader.innerText = signInHeader;
   createNameForm(signInText, true);
+
   function goToSignUp() {
-    target.innerHTML = ''
-    createSignupPage()
+    loginBase.innerHTML = '';
+    createSignupPage();
   }
-  createButton(signUpButtonText, () => goToSignUp())
+  createButton(signUpButtonText, () => goToSignUp());
 }
 
 /**
  * Creates the sign-up page with screen name form, extra form, and a button that takes user to sign-in page.
  */
 function createSignupPage() {
-  header.innerText = signUpHeader
+  loginHeader.innerText = signUpHeader;
   createNameForm(signUpText, false);
-  function returnToSignIn(){
-    target.innerHTML = ''
-    createSigninPage()
+
+  function returnToSignIn() {
+    loginBase.innerHTML = '';
+    createSigninPage();
+
   }
-  createButton(signInButtonText, ()=> returnToSignIn())
+  createButton(signInButtonText, () => returnToSignIn());
 }
 
 
