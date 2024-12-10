e196c51d7366938812a04812470bdebbc9b1cbba
MustBeViable <elias1rinne@gmail.com>
2024-12-10 15:44:12 +0200

    added stuff :---DDD

--- Game/Javascript/game_plays_here.js
+++ Game/Javascript/game_plays_here.js
@@ -1,8 +1,10 @@
 'use strict';
 
-async function game_plays_here (ide) {
-  await taxfree(ide)
-  await garbage_action(ide)
-  await hole_searcher(ide)
-  await airport_selection_function(ide)
+async function game_plays_here(ide) {
+  await airport_buttons();
+  let [airportMarkers, map] = await map_js_juttu();
+  await airport_selection_function(ide, airportMarkers, map);
+  await hole_search_buttons(ide)
 }
+
+game_plays_here(1);
