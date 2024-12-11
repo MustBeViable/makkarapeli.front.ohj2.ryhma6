e007d97a297c9a311e1766d3c17b890643337351
Aleksi <aleklap@metropolia.fi>
2024-12-11 15:36:28 +0200

    commit

--- Game/Javascript/map.js
+++ Game/Javascript/map.js
@@ -17,6 +17,18 @@ const map = L.map('map_here', {tap: false});
 
   console.log('end');
 /**
+  const mymakkaraicon = L.Icon({
+    iconUrl:'../images_and_other/logo.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+  })
+**/
+/**
  *     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
  *     <script src="https://unpkg.com/leaflet/dist/leaflet.js" defer></script>
  *     <script src="/Game/Javascript/map.js" defer></script>
