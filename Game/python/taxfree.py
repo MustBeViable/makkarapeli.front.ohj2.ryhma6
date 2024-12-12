fa4fd0610eacbc27b2dfba88f7ae3c349630ff66
senja <senja.vuoristo@aalto.fi>
2024-12-12 04:44:49 +0200

    Makkara score gets lower

--- Game/python/taxfree.py
+++ Game/python/taxfree.py
@@ -1,3 +1,4 @@
+from Game.python.count_makkara_score import count_makkara_score
 from Game.python.game_texts import sausage_price
 from Game.python.sql_querys.score_fetch_and_score_update_querys import player_score_fetch, player_score_update
 from Game.python.sql_querys.makkara_taxfree_sql_update import add_makkara_reached
@@ -11,7 +12,7 @@ def tax_free(player_money, makkara_id, game_id):
         add_makkara_reached(game_id, makkara_id)
         new_money = int(player_money) - sausage_price
         update_player_money(new_money, game_id)
-        new_player_score = player_score_fetch(game_id) + search_makkara_score(game_id)
+        new_player_score = player_score_fetch(game_id) + count_makkara_score(game_id, makkara_id)
         player_score_update(new_player_score, game_id)
         result = {'makkara': 'ostettu'}
         return result
